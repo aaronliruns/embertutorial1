23506540a3a41cb203d1830aabe9963d2cd31ade
developer <lichenlc@gmail.com>
2017-08-15 23:08:50 +0800

    firebase

--- config/environment.js
+++ config/environment.js
@@ -7,6 +7,22 @@ module.exports = function(environment) {
     environment,
     rootURL: '/',
     locationType: 'auto',
+
+    firebase: {
+      apiKey: 'AIzaSyAACDu8oaXOTXGDz0tTceewELzDBIMJ-OA',
+      authDomain: 'embertutorial1-58186.firebaseapp.com',
+      databaseURL: 'https://embertutorial1-58186.firebaseio.com',
+      storageBucket: '',
+      messagingSenderId: '147540413277'
+    },
+
+    // if using ember-cli-content-security-policy
+    contentSecurityPolicy: {
+      'script-src': "'self' 'unsafe-eval' apis.google.com",
+      'frame-src': "'self' https://*.firebaseapp.com",
+      'connect-src': "'self' wss://*.firebaseio.com https://*.googleapis.com"
+    },
+
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
