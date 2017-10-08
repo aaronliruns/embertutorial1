f9a5db6f3a8baf1831fdee06e384dc1c278ccee5
developer <lichenlc@gmail.com>
2017-10-08 22:19:59 +0800

    added ember send action diagram

--- app/components/library-item-form.js
+++ app/components/library-item-form.js
@@ -4,7 +4,9 @@ export default Ember.Component.extend({
   buttonLabel: 'Save',
 
   actions: {
-
+    //When a component is used inside a template, 
+    //it has the ability to send actions to that template's controller and routes.
+    //Here param == item == model == library
     buttonClicked(param) {
       this.sendAction('action', param);
     }
